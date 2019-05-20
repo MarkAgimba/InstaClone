ae671b230ee62133a70a94a2cb384b036565d454
Mark Agimba <markaguero16@gmail.com>
2019-05-20 14:22:54 +0300

    add admin view

--- clone/admin.py
+++ clone/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Profile, Image, Comments
 
 # Register your models here.
--- insta/settings.py
+++ insta/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'clone',
-    'bootstrap3'
+    # 'bootstrap3'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'minsta',
+        'USER': 'mark',
+        'PASSWORD': '12345'
     }
 }
 
@@ -120,3 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+#Login Redirecting to Home
+LOGIN_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
