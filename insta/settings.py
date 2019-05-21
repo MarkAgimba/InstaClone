62e34e796fceab178de431925e8cb56ce5a592e2
Mark Agimba <markaguero16@gmail.com>
2019-05-21 15:15:16 +0300

    email settings corrected

--- insta/settings.py
+++ insta/settings.py
@@ -139,3 +139,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # django_heroku.settings(locals())
 
 STATICFILES_DIRS= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='markagimba@gmail.com'
+EMAIL_HOST_PASSWORD='moroto12'
